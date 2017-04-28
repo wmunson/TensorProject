1ff9d63c7c96dd5b774b36a8a5d430213285c1bd
Wes <munsonwa@gmail.com>
2017-04-28 14:14:16 -0400

    readme update

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, request, render_template
 from config import DevConfig
 from model import make_test_csv, run_analysis
 import json
-
+from flask_cors import CORS, cross_origin
 
 
 
 app = Flask(__name__)
-
+CORS(app)
 app.config.from_object("config.DevConfig")
 
 # db = SQLAlchemy(app)
